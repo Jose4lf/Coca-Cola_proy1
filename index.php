c322361550219130679db8f18dc766d24853ceaf
Jose4lf <nighsoul7@gmail.com>
2023-10-14 13:34:51 -0400

    se agrego el proyecto

---
+++ index.php
@@ -0,0 +1,243 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8"> 
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="descripcion" content="Bienvenido a la página oficial de Coca-Cola, la icónica marca de refrescos que ha estado refrescando el mundo desde hace más de un siglo. En nuestro sitio web, te invitamos a explorar el emocionante mundo de Coca-Cola, donde la alegría y la refrescante felicidad se encuentran en cada sorbo.
+    Nuestra página te sumergirá en un viaje a través de la historia de Coca-Cola, desde su humilde origen en una pequeña farmacia en Atlanta hasta convertirse en una de las marcas más reconocidas y queridas a nivel mundial. Descubrirás cómo hemos evolucionado a lo largo de los años, manteniendo siempre la esencia de la autenticidad y la calidad que nos caracterizan.
+    Explora nuestra amplia gama de productos, desde la clásica Coca-Cola Original hasta las variantes más modernas y bajas en calorías. Encontrarás información detallada sobre los ingredientes y valores nutricionales de cada bebida para que puedas tomar decisiones informadas sobre tu consumo.
+    Además, te brindamos acceso a emocionantes promociones, concursos y eventos especiales que celebramos en todo el mundo. Descubre cómo Coca-Cola se involucra en la comunidad y apoya iniciativas sociales y ambientales para un mundo mejor.
+    En nuestra sección de recetas, te inspiraremos con deliciosas combinaciones de bebidas y cocteles que puedes crear con nuestros productos. ¡Haz que tus momentos especiales sean aún más inolvidables con nuestras sugerencias de bebidas refrescantes!
+    ¿Te interesa conocer cómo Coca-Cola contribuye a la sostenibilidad y al cuidado del medio ambiente? Visita nuestra sección de responsabilidad corporativa para descubrir nuestros esfuerzos en la reducción de residuos, la conservación del agua y otras iniciativas eco-amigables.
+    No importa si eres un apasionado seguidor de Coca-Cola o simplemente te encanta disfrutar de una bebida refrescante de vez en cuando; nuestro sitio web tiene algo para todos. Explora, diviértete y comparte tu amor por Coca-Cola con nosotros. ¡Gracias por ser parte de nuestra historia!">
+    <meta name="keywords" content="bebida,gaseosa,cola">
+    <title>Inicio - Coca Cola</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Caveat:wght@700&family=Roboto:ital@1&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="css/necolas.github.io_normalize.css_8.0.1_normalize.css">
+    <link rel="shortcut icon " href="img/favicon.png">
+    <link rel="stylesheet" href="style.css">
+    <script src="https://kit.fontawesome.com/8d5d475594.js" crossorigin="anonymous"></script>
+</head>
+<body>
+      <header>
+        <div class="container">
+            <a href="index.html"><img src="img/logo.svg" alt="logo" class="logo"></a>
+            <nav>
+                <a href="#Inicio">Inicio</a>
+                <a href="#Nosotros">Nosotros</a>
+                <a href="#Servicios">Servicios</a>
+                <a href="#Galeria">Galeria</a>
+                <a href="#Contactenos">Contactenos</a>
+
+            </nav>
+            <a href="#" class="hamb"><i class="fa-solid fa-bars"></i></a>
+        </div>
+      </header>
+      <main>
+        <section id="Inicio">
+            <img src="img/bannerprincipal.jpg" alt="Banner">
+            <div class="bloque-inicio">
+                <h1>Bienvenidos a Coca Cola</h1>
+                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ad, tenetur debitis! Officia, odio, illum voluptatibus cum placeat possimus impedit incidunt similique ratione voluptatem eligendi blanditiis eos laboriosam ullam aliquam nemo.                    
+                </p>
+                <a href="#Nosotros" class="boton boton-rojo">Ver Mas</a>
+            </div>
+        </section>
+        <section id="Nosotros" class="seccion">
+            <div class="container">
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum architecto magnam voluptate unde, mollitia inventore consequatur laudantium dolor reiciendis est, possimus aliquid ex ad dicta cum doloremque magni neque quis! </p>
+            </div>
+        </section> 
+        <section id="Servicios" class="seccion">
+            <div class="container">
+                <div class="row">
+                    <div class="column column-33 column-mobile-100">
+                        <div class="bloque-servicio">
+                            <div class="bloque-img-servicios cuadrado-perfecto">
+                                <img src="img/servicio1.jpg" alt="servicios1">
+                            </div>
+                            <div class="bloque-conteido-servcio ">
+                                <h1>Servicio1</h1>
+                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta ducimus ullam earum exercitationem debitis voluptas, dolorum laborum, accusantium eos iste molestias hic dolor, accusamus veniam. Voluptatibus, asperiores voluptate! Odio, aspernatur.
+                                </p>
+                                <a href="#" class="boton boton-blanco">Ver mas</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="column column-33 column-mobile-100">
+                        <div class="bloque-servicio">
+                            <div class="bloque-img-servicios cuadrado-perfecto">
+                                <img src="img/servicio2.jpg" alt="servicios2">
+                            </div>
+                            <div class="bloque-conteido-servcio">
+                                <h1>Servicio2</h1>
+                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta ducimus ullam earum exercitationem debitis voluptas, dolorum laborum, accusantium eos iste molestias hic dolor, accusamus veniam. Voluptatibus, asperiores voluptate! Odio, aspernatur.
+                                </p>
+                                <a href="#" class="boton boton-blanco">Ver mas</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="column column-33 column-mobile-100">
+                        <div class="bloque-servicio">
+                            <div class="bloque-img-servicios cuadrado-perfecto">
+                                <img src="img/servicio3.jpg" alt="servicios3">
+                            </div>
+                            <div class="bloque-conteido-servcio">
+                                <h1>Servicio3</h1>
+                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta ducimus ullam earum exercitationem debitis voluptas, dolorum laborum, accusantium eos iste molestias hic dolor, accusamus veniam. Voluptatibus, asperiores voluptate! Odio, aspernatur.
+                                </p>
+                                <a href="#" class="boton boton-blanco">Ver mas</a>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </section>      
+        <section id="Galeria">
+            <div class="container-fluid">
+                <div class="row">
+                    <div class=" column-33 column-mobile-50">
+                        <div class="cuadrado-perfecto">
+                            <img src="img/servicio1.jpg" alt="">
+                            <h4>Imagen1</h4>
+                        </div>
+                    </div>
+                    <div class=" column-33 column-mobile-50">
+                        <div class="cuadrado-perfecto">
+                            <img src="img/servicio2.jpg" alt="">
+                            <h4>Imagen2</h4>
+                        </div>
+                    </div>
+                    <div class=" column-33 column-mobile-50">
+                        <div class="cuadrado-perfecto">
+                            <img src="img/servicio3.jpg" alt="">
+                            <h4>Imagen3</h4>
+                        </div>
+                    </div>
+                    <div class=" column-33 column-mobile-50">
+                        <div class="cuadrado-perfecto">
+                            <img src="img/servicio1.jpg" alt="">
+                            <h4>Imagen4</h4>
+                        </div>
+                    </div>
+                    <div class=" column-33 column-mobile-50">
+                        <div class="cuadrado-perfecto">
+                            <img src="img/servicio2.jpg" alt="">
+                            <h4>Imagen5</h4>
+                        </div>
+                    </div>
+                    <div class=" column-33 column-mobile-50">
+                        <div class="cuadrado-perfecto">
+                            <img src="img/servicio3.jpg" alt="">
+                            <h4>Imagen6</h4>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section id="Contactenos" class="seccion">
+            <iframe width="520" height="400" frameborder="0" src="https://maps.google.com/maps?width=520&amp;height=400&amp;hl=en&amp;q=%20La%20Paz+(Bolivia)&amp;t=&amp;z=12&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"></iframe>
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="column column-41 empujar-58 column-mobile-100 empujar-mobile-0 sinpadding-mobile">
+                        <form action="index.php" method="post">
+                            <div class="form-block">
+                                <input type="text" name="nombre" class="form-control" placeholder="Nombre" >
+                            </div>
+                            <div class="form-block">
+                                <input type="email" name="email" class="form-control" placeholder="Email" >
+                            </div>
+                            <div class="form-block">
+                                <textarea  name="mensaje" placeholder="Mensaje"></textarea>
+                            </div>
+                            <div class="form-block bloque-ultimo">
+                                <input type="submit" class="boton boton-negro" value="Enviar" >
+                            </div>
+                            <?php 
+                                if ($_SERVER["REQUEST_METHOD"] == "POST") {
+                                    $nombre = $_POST["nombre"];
+                                    $email = $_POST["email"];
+                                    $mensaje = $_POST["mensaje"];
+                                
+                                    if (isset($nombre)) {
+                                        if (isset($email)) {
+                                            if (isset($mensaje)) {
+                                                $para = "nuevocorreo17@gmail.com";
+                                                $asunto = "esto es prueba";
+                                                $cuerpo = $nombre."\n".$email."\n".$mensaje;
+                                                $adicinar = "From: nighsoul7@gmail.com";
+
+                                                mail($para,$asunto,$cuerpo,$adicinar);
+                                            ?>
+                                                <p>Envio Existoso</p>
+                                            <?php
+                                            }
+                                        }
+                                    }
+                                }
+                            ?>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </section>
+      </main>
+      <footer class="seccion">
+        <div class="container">
+            <div class="row">
+                <div class="column column-25 column-mobile-100">
+                    <img src="img/logo-blanco.png" class="logo-footer" alt="">
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque natus sequi libero sit autem. Corrupti tempora possimus pariatur, quasi excepturi voluptate qui facere aperiam facilis nostrum nihil! Harum, ab ipsa.</p>
+                </div>
+                <div class="column column-25 column-mobile-100">
+                    <h3>
+                        Temas Relacionados
+                    </h3>
+                    <ul>
+                        <li><a href="#">Tema1</a></li>
+                        <li><a href="#">Tema2</a></li>
+                        <li><a href="#">Tema3</a></li>
+                    </ul>
+                </div>
+                <div class="column column-25 column-mobile-100">
+                    <h3>
+                        Datos de Contacto
+                    </h3>
+                    <ul>
+                        <li><a href="#">nighsoul7@gmail.com</a></li>
+                        <li><a href="#">62140433</a></li>
+                        <li><a href="#">av.Buenos Aires 277 - El Alto <br>La Paz - Bolivia</a></li>
+                    </ul>
+                </div>
+                <div class="column column-25 column-mobile-100">
+                    <h3>
+                        Redes Sociales
+                    </h3>
+                    <ul class="redes">
+                        <li><a href="#">
+                            <i class="fa-brands fa-facebook"></i>
+                            </a>
+                        </li>
+                        <li><a href="#">
+                            <i class="fa-brands fa-instagram"></i>
+                            </a>
+                        </li>
+                        <li><a href="#">
+                            <i class="fa-brands fa-twitter"></i>
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+        <div class="barra-footer">&copy;Derechos Reservados 2023</div>
+
+      </footer>
+      <script src="js/code.jquery.com_jquery-3.7.1.min.js"></script>
+      <script src="js/funciones.js"></script>
+    </body>
+</html>
